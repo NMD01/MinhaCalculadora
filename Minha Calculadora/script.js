4f27c9506defc1f554af362847fca39d0a0edef2
Nathan Martini Duarte <77181059+NMD01@users.noreply.github.com>
2021-05-20 12:39:57 -0300

    Add files via upload

--- Minha Calculadora/script.js
+++ Minha Calculadora/script.js
@@ -1,36 +1,39 @@
-
-
 let res = document.getElementById("res")
 
 
 res.readOnly = true
 
 
-document.addEventListener("keydown", enter = function(event){
-    let tecla = event.keyCode
-    
-    if(tecla == 13){
-        resultado()
-    }
-})
-
-
-// document.addEventListener("keydown", tn1 = function(event){
-//     let tecla = event.keyCode
-
-//     console.log(tecla)
-// })
-
-teclado = function(codigo, função){
+teclado = function(codigo, função, nome){
     document.addEventListener("keydown", teclaEvent = function(event){
         
         let tecla = event.keyCode
 
+        cor = function(){
+            nome.style.color = "white"
+            nome.style.backgroundColor = "black"
+        }
+        
         if(tecla == codigo){
             função()
+            nome.style.color = "black"
+            nome.style.backgroundColor = "white"
+
+            setTimeout(cor, 100)
         }
     })
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -41,72 +44,72 @@ um.addEventListener("click", n1 = function(){
     res.focus()
     return escrever(1)   
 })
-teclado(97, n1)
-teclado(49, n1)
+teclado(97, n1, um)
+teclado(49, n1, um)
 
 let dois = document.getElementById("dois")
 dois.addEventListener("click", n2 = function(){
     res.focus()
     return escrever(2)
 })
-teclado(98, n2)
-teclado(50, n2)
+teclado(98, n2, dois)
+teclado(50, n2, dois)
 
 let treis = document.getElementById("treis")
 treis.addEventListener("click", n3 = function(){
     res.focus()
     return escrever(3)
 })
-teclado(99, n3)
-teclado(51, n3)
+teclado(99, n3, treis)
+teclado(51, n3, treis)
 
 let quatro = document.getElementById("quatro")
 quatro.addEventListener("click", n4 = function(){
     res.focus()
     return escrever(4)
 })
-teclado(100, n4)
-teclado(52, n4)
+teclado(100, n4, quatro)
+teclado(52, n4, quatro)
 
 let cinco = document.getElementById("cinco")
 cinco.addEventListener("click", n5 = function(){
     res.focus()
     return escrever(5)
 })
-teclado(101, n5)
-teclado(53, n5)
+teclado(101, n5, cinco)
+teclado(53, n5, cinco)
 
 let seis = document.getElementById("seis")
 seis.addEventListener("click", n6 = function(){
     res.focus()
     return escrever(6)
 })
-teclado(102, n6)
-teclado(54, n6)
+teclado(102, n6, seis)
+teclado(54, n6, seis)
 
 let sete = document.getElementById("sete")
 sete.addEventListener("click", n7 = function(){
     res.focus()
     return escrever(7)
 })
-teclado(103, n7)
-teclado(55, n7)
+teclado(103, n7, sete)
+teclado(55, n7, sete)
 
 let oito = document.getElementById("oito")
 oito.addEventListener("click", n8 = function(){
     res.focus()
     return escrever(8)
 })
-teclado(104, n8)
-teclado(56, n8)
+teclado(104, n8, oito)
+teclado(56, n8, oito)
 
 let nove = document.getElementById("nove")
 nove.addEventListener("click", n9 = function(){
     res.focus()
     return escrever(9)
 })
-teclado(105, n9)
-teclado(57, n9)
+teclado(105, n9, nove)
+teclado(57, n9, nove)
 
 
 let zero = document.getElementById("zero")
@@ -114,8 +117,8 @@ zero.addEventListener("click", n0 = function(){
     res.focus()
     return escrever(0)
 })
-teclado(96, n0)
-teclado(48,n0)
+teclado(96, n0, zero)
+teclado(48,n0, zero)
 
 
 
@@ -128,30 +131,30 @@ mais.addEventListener("click", adição = function(){
     res.focus()
     return escrever(" + ")
 })
-teclado(107, adição)
+teclado(107, adição, mais)
 
 let menos = document.getElementById("menos")
 menos.addEventListener("click", subtração = function(){
     res.focus()
     return escrever(" - ")
 })
-teclado(109, subtração)
-teclado(189, subtração)
+teclado(109, subtração, menos)
+teclado(189, subtração, menos)
 
 let dividir = document.getElementById("dividir")
 dividir.addEventListener("click", divisão = function(){
     res.focus()
     return escrever(" / ")
 })
-teclado(111, divisão)
+teclado(111, divisão, dividir)
 
 let vezes = document.getElementById("vezes")
 vezes.addEventListener("click", multiplicação = function(){
     res.focus()
     return escrever(" * ")
 })
-teclado(106, multiplicação)
-teclado(88, multiplicação)
+teclado(106, multiplicação, vezes)
+teclado(88, multiplicação, vezes)
 
 let igual = document.getElementById("igual")
 igual.addEventListener("click", resultado = function(){
@@ -166,25 +169,26 @@ igual.addEventListener("click", resultado = function(){
 
 
 })
-teclado(187, resultado)
+teclado(187, resultado, igual)
+teclado(13, resultado, igual)
 
 let limpar = document.getElementById("limpar")
 limpar.addEventListener("click", limparConta = function(){
     res.focus()
     res.value = ""
 })
-teclado(46, limparConta)
-teclado(8, limparConta)
+teclado(46, limparConta, limpar)
+teclado(8, limparConta, limpar )
 
 let virgula = document.getElementById("virgula")
 virgula.addEventListener("click", colocarVirgula = function(){
     res.focus()
     return escrever(".")
 })
-teclado(110, colocarVirgula)
-teclado(108, colocarVirgula)
-teclado(188, colocarVirgula)
-teclado(190, colocarVirgula)
+teclado(110, colocarVirgula, virgula)
+teclado(108, colocarVirgula, virgula)
+teclado(188, colocarVirgula, virgula)
+teclado(190, colocarVirgula, virgula)
 
 
 
